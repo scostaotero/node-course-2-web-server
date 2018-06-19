775ae57fa830a35ae2f08628d7346261d57c6a33
Sara Costa <s.costa.otero@accenture.com>
2018-06-19 09:35:13 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app=express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -50,4 +51,7 @@ app.get('/bad',(req,res)=>{
     res.send({errorMessage:'Unable to fulfill the request.'});
 });
 
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}.`);
+});
+
